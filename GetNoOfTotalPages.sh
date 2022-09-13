ee9fb6574ed7ccf0149080f8be87306f3e7567ee
dhanushkaEkanayake <113347736+dhanushkaEkanayake@users.noreply.github.com>
2022-09-13 13:13:24 +0530

    Update GetNoOfTotalPages.sh

--- GetNoOfTotalPages.sh
+++ GetNoOfTotalPages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-GIT_API="https://git.zone24x7.lk/api/v4"
-GIT_TOKEN="bVJ9jFDovV6vUEg-UmCt"
-GROUP_ID="1079"
+GIT_API="your_api_url"
+GIT_TOKEN="your_personel_access_token"
+GROUP_ID="group_id"
 
 total_pages=$(curl -I --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/projects?include_subgroups=true&per_page=100&archived=false" | grep -Fi X-Total-Pages | sed 's/[^0-9]*//g')
 
