b684177def18d775cab8b32b81cbe09c85eeee2c
dhanushkaEkanayake <113347736+dhanushkaEkanayake@users.noreply.github.com>
2022-09-13 13:58:10 +0530

    Update UserRemove.sh

--- UserRemove.sh
+++ UserRemove.sh
@@ -1,61 +1,8 @@
 #! /bin/bash
 
-GIT_API="https://git.zone24x7.lk/api/v4"
-GIT_TOKEN="bVJ9jFDovV6vUEg-UmCt"
-#GROUP_NAME="testdevops"
-GROUP_NAME=$1
-
-if [ "$1" == "" ] || [ $# -gt 1 ];
-then
-        echo -e "\n Please try with valid Group Name...!! \n"
-else
-
-
-#----------------------------------------------------------------------------------------Retrieve the Group ID--------------------------------------------------------------------------------------------
-
-	GROUP_ID=$(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" -g "$GIT_API/groups?top_level_only=true&search=${GROUP_NAME}" | jq -r ".[] .id")
-
-	echo -e "\n [-----------------------------Group Member delete Process Started---------------------------------] \n\n The selected Group Name is: ${GROUP_NAME} and ID is: ${GROUP_ID}\n"
-
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-#--------------------------------------------------------------------Retrieve the User ID and Access Levels of members in the Group------------------------------------------------------------------------
-
-	USER_ID_ARRAY=($(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members" | jq -r ".[] .id"))
-
-	USER_ACCESS_LEVEL_ARRAY=($(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members" | jq -r ".[] .access_level"))
-
-	#echo ${USER_ACCESS_LEVEL_ARRAY[*]}
-
-	echo -e "\n Following members are in the group \n "
-
-	for (( i=0; i<${#USER_ID_ARRAY[@]}; i++ ))
-	do
-        	echo -e " ${USER_ID_ARRAY[$i]}"
-	done
-
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-#-----------------------------------------------------------------------------Determine the Non owner member----------------------------------------------------------------------------------------------
-
-	for (( c=0; c<${#USER_ACCESS_LEVEL_ARRAY[@]}; c++ ))
-	do
-        	if [ ${USER_ACCESS_LEVEL_ARRAY[$c]} != 50 ]
-		then
-		NON_OWNER_USERS_ID_ARRAY+=(${USER_ID_ARRAY[$c]})
-		fi
-#! /bin/bash
-
-GIT_API="https://git.zone24x7.lk/api/v4"
-GIT_TOKEN="bVJ9jFDovV6vUEg-UmCt"
-#GROUP_NAME="testdevops"
-GROUP_NAME=$1
+GIT_API="your_api_url"
+GIT_TOKEN="your_personel_access_token"
+GROUP_NAME="$1"
 
 if [ "$1" == "" ] || [ $# -gt 1 ];
 then
