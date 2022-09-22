d227739086316baf02ffbf771940203f19dfd4d6
dhanushka <dhanushka@serverlab.com>
2022-09-22 12:04:12 -0700

    Initial Commit

---
+++ DeleteMultipleProjects.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GIT_API="http://gitlab.serverlab.com/api/v4"
+GIT_TOKEN="glpat-MLCucPiBYG9TWZhToa9Z"
+GROUP_ID="12"
+
+for (( e=20; e<150; e++ ))
+do
+        curl\
+                --request DELETE\
+                --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/projects?name=Project${e}&namespace_id=${GROUP_ID}"
+
+done
