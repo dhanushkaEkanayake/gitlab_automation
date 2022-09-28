906cd65a4f802b4ce7d92cbed67e827883feb6e0
dhanushka <dhanushka@serverlab.com>
2022-09-28 11:47:07 -0700

    commit

--- createProject/CreateProjectsWithSampleNames.sh
+++ createProject/CreateProjectsWithSampleNames.sh
@@ -4,10 +4,11 @@ GIT_API="http://gitlab.serverlab.com/api/v4" 	#replace with your api url
 GIT_TOKEN="glpat-MLCucPiBYG9TWZhToa9Z"    #replace with your personal access token
 GROUP_ID="12"    #replace with desired group ID
 
-for (( e=0; e<100; e++ ))  #no.of new projects required
-do
-        curl\
-                --request POST\
-                --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/projects?name=Project${e}&namespace_id=${GROUP_ID}"
+	readarray -t name < sampleProjectNames.txt
 
-done
+	for i in ${name[@]}
+	do
+        	curl\
+                --request POST\
+                --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/projects?name=${i}&namespace_id=${GROUP_ID}"
+	done
