d97e7914bf413f75fc38035fbc71b12cf5414ecc
dhanushka <dhanushka@serverlab.com>
2022-09-29 02:49:46 -0700

    Commit

--- UserRemove.sh
+++ UserRemove.sh
@@ -1,80 +1,91 @@
 #! /bin/bash
 
-GIT_API="your_api_url"
-GIT_TOKEN="your_personel_access_token"
-GROUP_NAME="$1"
+GIT_API="http://gitlab.serverlab.com/api/v4"	#replace with your api url
+GIT_TOKEN="glpat-MLCucPiBYG9TWZhToa9Z"	#replace with your personal access token
+GROUP_NAME="test_one"
 
-if [ "$1" == "" ] || [ $# -gt 1 ];
-then
-        echo -e "\n Please try with valid Group Name...!! \n"
-else
-
-
-#----------------------------------------------------------------------------------------Retrieve the Group ID--------------------------------------------------------------------------------------------
-
-	GROUP_ID=$(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" -g "$GIT_API/groups?top_level_only=true&search=${GROUP_NAME}" | jq -r ".[] .id")
-
-	echo -e "\n [-----------------------------Group Member delete Process Started---------------------------------] \n\n The selected Group Name is: ${GROUP_NAME} and ID is: ${GROUP_ID}\n"
-
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-#--------------------------------------------------------------------Retrieve the User ID and Access Levels of members in the Group------------------------------------------------------------------------
-
-	USER_ID_ARRAY=($(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members" | jq -r ".[] .id"))
-
-	USER_ACCESS_LEVEL_ARRAY=($(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members" | jq -r ".[] .access_level"))
-
-	#echo ${USER_ACCESS_LEVEL_ARRAY[*]}
-
-	echo -e "\n Following members are in the group \n "
-
-	for (( i=0; i<${#USER_ID_ARRAY[@]}; i++ ))
+	
+	#-----To get the no of group result pages
+	total_pages=$(curl -I --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups?owned=true&top_level_only=true&per_page=100" | grep -Fi X-Total-Pages | sed 's/[^0-9]*//g')
+    
+    
+	for (( d=1; d<=${total_pages+1}; d++ ))
 	do
-        	echo -e " ${USER_ID_ARRAY[$i]}"
-	done
+    		#------To retrieve the corresspondance group name for the given group name
+		GROUP_NAME_DUMP+=$(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN"\
+ 								-g "$GIT_API/groups?owned=true&top_level_only=true&per_page=100&page=${d}&search="$GROUP_NAME"" | jq -r ".[] .name")
+    
+    		if [[ "${GROUP_NAME_DUMP}" == "${GROUP_NAME}"  ]]; #-------Comapare the correspondance group name with given group name
+		then
+			GROUP_ID=$(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN"\
+                        	-g "$GIT_API/groups?owned=true&top_level_only=true&per_page=100&page=${d}&search=$GROUP_NAME" | jq -r ".[] .id")
+ 		fi      
+    	done
+    
+    	#----Un comment this to print the retrieved group ID------
+	#	echo -e " "${GROUP_ID}""
+	#----------------------------------------------------------
+    
+    	if [ -z "${GROUP_ID}" ] #-------Check whether is there any group for the given group name or not
+    	then
+    		echo -e "\n Invalid Group Name...!! \n"
+        	exit 1
+    	fi
 
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	
+
+	#-------To get the no of members result pages
+    	total_pages=$(curl -I --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members?per_page=100" | grep -Fi X-Total-Pages | sed 's/[^0-9]*//g')
+   
+   
+    	for (( d=1; d<=${total_pages+1}; d++ ))
+	do
+    		#------Retrieve the user ids and their access levels of the members in the group
+   		USER_ID_ARRAY+=($(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members?per_page=100&page=${d}" | jq -r ".[] .id"))
+        	USER_ACCESS_LEVEL_ARRAY+=($(curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/groups/$GROUP_ID/members?per_page=100&page=${d}" | jq -r ".[] .access_level"))
+    	done	
+    
+
+	#-------Un comment this to print retrieved user IDs----------
+		echo ${USER_ID_ARRAY[*]}
+	#------------------------------------------------------------
+    
+    	#---Un comment this to print retrieved user access levels----
+		echo ${USER_ACCESS_LEVEL_ARRAY[*]}
+	#------------------------------------------------------------
 
 
-
-
-#-----------------------------------------------------------------------------Determine the Non owner member----------------------------------------------------------------------------------------------
-
-	for (( c=0; c<${#USER_ACCESS_LEVEL_ARRAY[@]}; c++ ))
+	
+	
+	for (( c=0; c<${#USER_ACCESS_LEVEL_ARRAY[@]}; c++ )) #------Determine the user IDs of the members without ownership
 	do
         	if [ ${USER_ACCESS_LEVEL_ARRAY[$c]} != 50 ]
 		then
-		NON_OWNER_USERS_ID_ARRAY+=(${USER_ID_ARRAY[$c]})
+			MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY+=(${USER_ID_ARRAY[$c]})
+            		MEMBERS_WITHOUT_OWNERSHIP_USER_ACCESS_LEVEL_ARRAY+=(${USER_ACCESS_LEVEL_ARRAY[$c]})
 		fi
 	done
 
-	
-	echo -e "\n Following members will be removed from the group \n "
+	if [ -z "${MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY}" ]; #------Determine there are members to be remove or not
+	then
+		echo -e "No members to remove "
+	else
 
+		for (( e=0; e<${#MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY[@]}; e++ )) #------Remove the members 
+        	do
+             		curl -sS --request DELETE --header "PRIVATE-TOKEN: $GIT_TOKEN" -g "$GIT_API/groups/$GROUP_ID/members/${MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY[$e]}"
+        	done
 
-	for (( d=0; d<${#NON_OWNER_USERS_ID_ARRAY[@]}; d++ ))
-	do
-        	echo -e " ${NON_OWNER_USERS_ID_ARRAY[$d]}"
-	done
+		echo -e " Following Members are removed "
+		
+		for (( d=0; d<${#MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY[@]}; d++ )) #-------Retrieve the name of the members without ownership
+        	do
+              		curl -sS --request GET --header "PRIVATE-TOKEN: $GIT_TOKEN" "$GIT_API/users?id=${MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY[d]}" | jq -r ".[] .name"
+		done
+        
+        	printf "%s\n" "${MEMBERS_WITHOUT_OWNERSHIP_USER_ID_ARRAY[@]}" > ${GROUP_ID}_userID.txt
 
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        	printf "%s\n" "${MEMBERS_WITHOUT_OWNERSHIP_USER_ACCESS_LEVEL_ARRAY[@]}" > ${GROUP_ID}_userAccessLevel.txt
 
-
-
-
-
-#----------------------------------------------------------------------------------------Removing Members------------------------------------------------------------------------------------------------
-
-	for (( e=0; e<${#NON_OWNER_USERS_ID_ARRAY[@]}; e++ ))
-	do
-        	curl -sS --request DELETE --header "PRIVATE-TOKEN: $GIT_TOKEN" -g "$GIT_API/groups/$GROUP_ID/members/${NON_OWNER_USERS_ID_ARRAY[$e]}"
-	done
-
-	echo -e "\n SuccessFull!!\n"
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-fi
+	fi
